2f2cdbe0c01cda754187df66149d40d8f363a9c2
Filip Komorowski <43729880+ItachiEU@users.noreply.github.com>
2020-10-02 15:03:51 +0200

    Update trie_dynamic.cpp

--- trie_dynamic.cpp
+++ trie_dynamic.cpp
@@ -1,60 +1,57 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-/* zakładamy że stringi mają tylko małe litery */
 
 class Trie{ 
 	private:
-		struct Node { 
+		struct Node {
+			Node(){
+				this->end = false;
+				for(int i = 0; i < 26; i++)
+					this->letters[i] = NULL;
+			}
 			Node *letters[26];
 			bool end;
 		};
 		Node* root;
 	public:
-		void setNewNode(Node* node) { 
-			for(int i = 0; i < 26; i++) 
-				node->letters[i] = NULL;
-			node->end = false;
-		} 
 		Trie() { 
-			root = (Trie::Node* )malloc(sizeof(Node));
-			setNewNode(root);
+			root = new Node();
 		}
-		void Add(string s) { 
-			AddIter(root, s, 0);		
-		}
-		void AddIter(Node* currNode, string &s, int i) { 
-			if(i == s.length()) { 
-				currNode->end = true;	
-				return;
+		void addHelper(Node* root, string s){
+			for(int i = 0; i <= s.size(); i++){
+				if(i == s.size()){
+					root->end = true;
+					return;
+				}
+				if(root->letters[s[i]-'a'] == NULL){
+					root->letters[s[i]-'a'] = new Node();
+					root = root->letters[s[i]-'a'];
+				}
+				else
+					root = root->letters[s[i]-'a'];
 			}
-			if(currNode->letters[s[i]-'a'] == NULL) { 
-				currNode->letters[s[i]-'a'] = (Trie::Node* )malloc(sizeof(Node));
-				setNewNode(currNode->letters[s[i]-'a']);
-			}
-			AddIter(currNode->letters[s[i]-'a'],s, i+1);
-		} 
-		bool check(string s){
-			checkHelper(root, s, 0);
 		}
-		bool checkHelper(Node* currNode, string &s, int i){
-			if(i == s.length())
-				return currNode->end == true;
-			if(currNode->letters[s[i]-'a'] == NULL)
-				return false;
-			else
-				checkHelper(currNode->letters[s[i]-'a'], s, i+1);
+		void add(string s){
+			addHelper(root, s);
+		}
+		bool check(string s){ // check if substring
+			return checkHelper(root, s);
+		}
+		bool checkHelper(Node* root, string s){
+			for(int i = 0; i < s.size(); i++){
+				if(root->letters[s[i]-'a'] == NULL)
+					return false;
+				root = root->letters[s[i]-'a'];
+			}
+			return true;
 		}
 };
 int main(){
 	Trie drzewko;
-	for(int i=0; i<10; i++){
-		int a; cin>>a;
-		string tekst;
-		cin>>tekst;
-		if(a == 1)
-			drzewko.Add(tekst);
-		else
-			cout<<drzewko.check(tekst);
-	}
+	string a;
+	cin >> a;
+	drzewko.add(a);
+	cout << drzewko.check("siema");
 	return 0;
 }
